a11c8a13d2c1dc77a6e394f007fb5ad8217921de
Anish Kanthamneni <109305222+akneni@users.noreply.github.com>
2024-06-16 19:11:45 -0400

    Remove RSA crate (use TLS protocol instead)

--- src-tauri/src/api_interface.rs
+++ src-tauri/src/api_interface.rs
@@ -1,13 +1,41 @@
 use reqwest;
 use anyhow::{anyhow, Result};
+use serde::{Serialize, Deserialize};
 
+pub async fn create_proj(endpoint: &str, name: &str, data: Vec<u8>) -> Result<()> {
+    let endpoint = format!("{}/proj/create-proj?name={}", endpoint, name);
 
-pub async fn create_proj(endpoint: &str) -> Result<()> {
-    let res = reqwest::get(endpoint)
+    let client = reqwest::Client::new();
+    let res = client.post(endpoint)
+        .body(data)
+        .send()
+        .await?;
+    
+    let res = res.bytes().await.unwrap();
+    let res = res.to_vec();
+
+    #[derive(Debug, Serialize, Deserialize)]
+    enum AgentResponse {
+        Ok(String),
+        OkData(Vec<u8>),
+        AgentErr(String),
+        ClientErr(String),
+    }
+
+    let res: AgentResponse = bincode::deserialize(&res).unwrap();
+
+    println!("{:?}", res);
+
+    Ok(())
+}
+
+pub async fn get_pub_key(endpoint: &str) -> Result<Vec<u8>> {
+    let endpoint = format!("{}/security/get-pub-key", &endpoint);
+    let res = reqwest::get(&endpoint)
         .await
         .map_err(|e| anyhow!("{}", e))?;
 
-    
+    let res = res.bytes().await.unwrap();
 
-    Ok(())
+    Ok(res.to_vec())
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,11 +3,21 @@
 
 mod api_interface;
 
+use tynkerbase_universal::{
+    crypt_utils::{
+        self, compression_utils, rsa_utils, BinaryPacket, CompressionType, RsaKeys
+    }, 
+    docker_utils, 
+    proj_utils::{self, FileCollection}
+};
+
 use std::fs;
 use std::path;
 use std::env;
 use std::sync::Mutex;
+use bincode;
 use sled;
+use tokio;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -16,7 +26,57 @@ fn greet(name: &str, state: tauri::State<'_, Mutex<sled::Db>>) -> String {
     format!("Hello, {}! You current path is", name)
 }
 
+async fn test() {
+    let res = api_interface::get_pub_key("http://192.168.101.241:7462")
+        .await;
+
+    let res = match res {
+        Ok(r) => r,
+        Err(e) => {
+            println!("Error: {e}");
+            std::process::exit(0);
+        }
+    };
+
+    let apikey = "tyb_key_xuJHXGzPGsK7AcdxAYJRdZQtWvDsBSdksR8lFDPFTMAxmMuTyeCuoJM8L1e2dDyt";
+
+    let mut packet = BinaryPacket::new();
+    packet.attach_key(&apikey).unwrap();
+
+    let packet = bincode::serialize(&packet).unwrap();
+
+    let res = api_interface::create_proj("http://192.168.101.241:7462", "test-proj-1", packet)
+        .await
+        .unwrap();
+
+
+    // let ig: Vec<String> = vec![];
+    // let files = FileCollection::load("/home/aknen/Documents/TCP/workspace-1/", &ig)
+    //     .unwrap();
+
+    // let mut packet = BinaryPacket::from(&files)
+    //     .unwrap();
+    
+    // packet.attach_key(&apikey)
+    //     .unwrap();
+    
+    // rsa_utils::encrypt(&mut packet, &pubkey)
+    //     .unwrap();
+
+    // let packet = bincode::serialize(&packet)
+    //     .unwrap();
+
+
+}
+
 fn main() {
+    let f = test();
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    rt.block_on(f);
+    return;
+
+
+
     let db = sled::open("./data.sled")
         .expect("Error opening local data");
     let db = Mutex::new(db);
